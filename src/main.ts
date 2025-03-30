feb652779eb2e92511516bcab4872ff1a26e316d
Hoangdev17 <hoangluudev17@gmail.com>
2025-03-30 22:17:51 +0700

    mfixa

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,6 @@ async function bootstrap() {
 
   app.enableCors();
 
-  app.useGlobalGuards(new JwtAuthGuard(reflector, jwtService));
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,27 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Put, Req, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { JwtAuthGuard } from 'src/common/guards/jwt.guard';
 
 @Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() dto: CreateTaskDto, @Req() req: any) {
     return this.taskService.create(dto, req.user.sub);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() dto: UpdateTaskDto, @Req() req: any) {
     return this.taskService.update(id, dto, req.user.sub);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string, @Req() req: any) {
     return this.taskService.delete(id, req.user.sub);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll(@Req() req: any) {
     return this.taskService.findAllByUser(req.user.sub);
